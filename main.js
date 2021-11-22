25d4da466b0629399c4c9059c5575baaeaa2aa1d
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-11-22 10:27:49 +0000

    (docs): Added a note to the main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const foodContainer = document.getElementById("food-container");
 // EXTRA - SOME OF OUR PRICES ARE NOT NUMBERS OR ARE 0
 const hasPriceArr = data.filter(foodItem => !isNaN(foodItem.price) && foodItem.price > 0);
 
+// I WROTE THIS AS A FUNCTION AS I MIGHT NEED IT LATER ON.
 const generateCardHTML = foodArray => {
   const HTML = foodArray.map(food => {
     return `<div><p>${food.img}</p><p>${food.name}</p><p>£${food.price}</p></div>`;
